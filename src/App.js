a76ccc67d9ab4ceaa65f612755f58c345c42d9ec
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-01-26 13:02:28 -0800

    updates imports in App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // Components
 import { Header } from './components/Navigation';
-import { Home } from './components';
 
 // Routes
 import SiteRoutes from './SiteRoutes';

6eb24cabf241429790e2c50325834c7e336bee27
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-01-26 18:10:35 -0800

    deletes README contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import Slider from 'react-slick';
+
+// styles
+import '../css/output.css';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  const settings = {
+    dots: true,
+    autoplay:true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+  return (
+    <Slider {...settings}>
+      <div>
+        <h1 className='text-center'>1</h1>
+      </div>
+      <div>
+        <h1 className='text-center'>2</h1>
+      </div>
+      <div>
+        <h1 className='text-center'>3</h1>
+      </div>
+      <div>
+        <h1 className='text-center'>4</h1>
+      </div>
+    </Slider>
+  )
 }
